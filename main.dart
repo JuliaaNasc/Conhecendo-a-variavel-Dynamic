8fee0d9ab48252fad97f39941fbac8eda173312f
Júlia Nascimento <juliavcostan@gmail.com>
2023-09-12 14:13:21 -0300

    Esse código funciona da seguinte forma: De inicio para da inicio ao aprendizado das variáveis como no exemplo a do tipo (DYNAMIC). Como no exemplo, é demostrada algumas formas de mostrar ao usuário uma determinada resposta.

---
+++ main.dart
@@ -0,0 +1,15 @@
+void main (){
+    
+  print('');  
+  dynamic dinamico = 'julia';
+  print ('ESSE TIPO DE VARIAVEL RECEBEU UM VALOR EQUIVALENTE A: ${dinamico}');
+  print ('');
+  
+  int numero = 5;
+  print ('O NÚMERO EQUIVALENTE RECEBIDO É: ${numero}');
+  print('----------------------------------------------------');
+  print('');
+  print ('COM A JUNÇÃO DE DOIS VALORES FICA: ${dinamico}' ' e ' '${numero}');
+  print('');
+
+}
